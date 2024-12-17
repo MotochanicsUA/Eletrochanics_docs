6e09a0dd4a9fcefed650cba43d66f1fb166a7a3f
fungame2270 <tomasvictal@gmail.com>
2024-12-17 20:33:38 +0000

    first commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/tomas/Documents/Moto/Eletrochanics_docs/node_modules/infima/dist/css/default/default.css"),
+  require("/home/tomas/Documents/Moto/Eletrochanics_docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/tomas/Documents/Moto/Eletrochanics_docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/tomas/Documents/Moto/Eletrochanics_docs/src/css/custom.css"),
+];
